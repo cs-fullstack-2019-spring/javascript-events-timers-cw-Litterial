4b66d7225fc30ae9e263a883ce3591aff86f23cf
Litterial <yungice12@gmail.com>
2019-02-07 09:38:04 -0600

    Challenge

--- Challenge.js
+++ Challenge.js
@@ -1,25 +1,51 @@
-/*
+
 var arrayofSprites=document.getElementsByTagName("img");
 var x=0;
+var blink=true;
 
 
-var SpriteImage=window.setInterval(myCallBack,1000);
-for(i=0;i<arrayofSprites.length;i++)
+
+var SpriteImage=window.setInterval(myCallBack,400);
+for(i=1;i<arrayofSprites.length;i++)
 {
-    arrayofSprites[i].setAttribute("hidden",true)
+    arrayofSprites[i].setAttribute('hidden',true)
 }
 
 function myCallBack()
 {
-    arrayofSprites[x].removeAttribute("hidden");
-    arrayofSprites[x].setAttribute("hidden",true);
-    x++;
-    if (x>9)
-        x=0;
-}*/
+    if(blink)   /*the purpose of this statement is to make the animations more crisp. This will pause after each animation*/
+    {
 
-var intervalID = window.setInterval(myCallback, 500);
 
-function myCallback() {
-    console.log("hi")
+        blink=false;  /*makes blink false*/
+    }
+
+    else         /*when blink is false*/
+    {
+
+
+
+        if (x==8)
+        {
+            console.log(x);
+
+            arrayofSprites[8].setAttribute('hidden',true);   /*for some reason a 2nd sprite of the last frame appears
+                                                                                 This disables it.*/
+            x=0;
+            arrayofSprites[0].removeAttribute('hidden');   /*unhides the first sprite*/
+            blink=true;
+        }
+
+        else {
+
+            arrayofSprites[x].removeAttribute("hidden");      /*unhides a sprite*/
+            console.log(x);
+            arrayofSprites[x].setAttribute('hidden', true);     /*hides a sprite*/
+            x++;                                                            /*change the index*/
+            arrayofSprites[x].removeAttribute('hidden');       /*makes a new sprite appear*/
+            blink = true;                                                   /*sets blink to false*/
+        }
+
+    }
 }
+
--- cw.js
+++ cw.js
@@ -2,18 +2,17 @@
 var angryMessage=document.getElementsByTagName('input');  /*looks for input tags*/
 angryMessage[0].addEventListener('click',angry);  /*array is set to zero to find the first input*/
 function angry(e) {       /*message when they click the checkbox*/
-    alert("I TOLD YOU NOT TO CLICK THIS!!!")
+    alert("I TOLD YOU NOT TO CLICK THIS!!!");
     e.preventDefault();  /*turns off the mouse*/
-
-
 
 }
 /*Question2...........................*/
-var enable=false;
+var enable=true;
 var question2Button=document.getElementsByTagName('button'); /*call all buttons*/
 var textArea=document.getElementsByTagName('textarea');  /*calls all elements with a textarea*/
 var hiddenMessage=document.getElementById('hide');   /*calls element with id 'hide'*/
 hiddenMessage.setAttribute('hidden',true);   /*sets the attribute to hidden*/
+textArea[0].setAttribute('disabled',true)
 question2Button[0].addEventListener('click',function()  /*this is the first button in the array*/
 {
 
@@ -22,15 +21,15 @@ question2Button[0].addEventListener('click',function()  /*this is the first butt
     if (enable)   /*if enable== false, skip to "else"*/
     {
          textArea[0].removeAttribute('disabled');   /*enables textarea*/
-         hiddenMessage.setAttribute('hidden',true);   /*hides message*/
+        hiddenMessage.removeAttribute('hidden',true);  /*unhides message*/
         enable=false;   /*makes enable false*/
     }
 
     else   /*disables text area, unhides the message*/
     {
         textArea[0].setAttribute('disabled',""); /*disables textarea*/
-        hiddenMessage.removeAttribute('hidden',true);  /*unhides message*/
 
+        hiddenMessage.setAttribute('hidden',true);   /*hides message*/
         enable=true;  /*makes enable true*/
     }
 
@@ -39,19 +38,6 @@ question2Button[0].addEventListener('click',function()  /*this is the first butt
 /*Question 3..................................................*/
 var countitDown=document.getElementById("countdown");  /*calls for element with id "countdown*/
 countitDown.addEventListener('click',liftoff);  /*click listener for button*/
-/*count=10;  /!*countdown counter*!/
-function liftoff(e)
-{
-    console.log('hi')
-    e.target.innerHTML=count;    /!*changes the text in the button*!/
-    count--;  /!*subtracts the count by 1 each iteration*!/
-    if (count<0)
-    {
-        e.target.innerHTML = "Liftoff";   /!*Liftoff prints when count==0*!/
-        count=10;    /!*restarts back to 10 if the user would like to try again*!/
-    }
-
-}*/
 
 
 function liftoff(e) {
@@ -67,14 +53,13 @@ function liftoff(e) {
             e.target.innerHTML = "Liftoff";
             console.log("Liftoff");/*to see what's happening in the console.log*/
             clearInterval(intervalID);
-        }
-        else {
+        } else {
             e.target.innerHTML = tentoLiftoff;/*Shows on button*/
             console.log(tentoLiftoff); /*see what's happening in the console.log*/
             tentoLiftoff--;     /*increments down*/
         }
     }
-
+}
 
 
 
